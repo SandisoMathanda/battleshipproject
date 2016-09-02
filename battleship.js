5686ae2a1bcb28051db55012ef96901308a4a25e
Sandiso Mathanda <sandisomathanda@gmail.com>
2016-09-02 13:20:33 -0700

    added numbers and letters to boxes

--- battleship.js
+++ battleship.js
@@ -20,6 +20,7 @@ var letterConversion = {
 	"I": 8,
 	"J": 9
 }
+var letterArray = ["A","B","C","D","E","F","G","H","I","J"];
 
 // makes the grid columns and rows
 for (i = 0; i < cols; i++) {
@@ -30,11 +31,15 @@ for (i = 0; i < cols; i++) {
 		gameBoardContainer.appendChild(square);
 
 
+
     // give each div element a unique id based on its row and column, like "s00"
 		square.id = 's' + j + i;
 		square.className = "boardSquare";
 
 		// THIS IS WHERE YOU WILL ADD CODE FOR PART 1 TO ADD TEXT TO EACH SQUARE
+    square.textContent =letterArray[j] + (i + 1) ;
+
+
 
 		// set each grid square's coordinates: multiples of the current row or column number
 		var topPosition = j * squareSize;
